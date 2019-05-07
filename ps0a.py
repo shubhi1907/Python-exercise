998e52e6e78126e9010496d28830c3da1742ae24
shubhi1907 <30296820+shubhi1907@users.noreply.github.com>
2019-05-07 13:23:44 -0400

    Assignment 0

---
+++ ps0a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 27 21:17:39 2018
+
+@author: shubhi
+"""
+
+import numpy
+x  = int(input("Enter a number x: "))
+y  = int(input("Enter a number y: "))
+print("x**y = ", x**y)
+print("log(x) = ", int(numpy.log2(x)))
+
+
